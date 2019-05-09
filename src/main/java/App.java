9be02eb8c7f09d7c2731c12f4dd20867cf33820b
Alena <alenka.konstantinovna@yandex.ru>
2019-05-09 15:45:51 +0300

    Рабочий вариант

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,50 +1,76 @@
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
-    private static Map<Metric, String> metricFileMap;
-
-    private static void fillMetricFileMap(String filePath) {
-        metricFileMap = new HashMap<Metric, String>();
-    }
+    private static Store store;
 
     public static void main(String[] args) throws IOException {
-        String filePath = args[0];
-        System.out.println(filePath);
-        fillMetricFileMap(filePath);
-        BufferedImage image = getImage(filePath);
-        Metric nearMetric = getNearMetric(image);
-        String fileName = metricFileMap.get(nearMetric);
-        XMPParams xmpParams = XMPParser.parse(new File(filePath + fileName));
-        System.out.println(xmpParams);
+        String sourceDirectory = "C:\\Users\\Алена\\Desktop\\diploma\\test2\\source";
+        String imageDirectory = "C:\\Users\\Алена\\Desktop\\diploma\\test2\\images";
+        fillStore(sourceDirectory);
+        processImages(imageDirectory);
     }
 
-    private static Metric getNearMetric(BufferedImage bufferedImage) {
-        Metric currentMetric = new Metric(bufferedImage);
-        int minDist = Integer.MAX_VALUE;
-        Metric result = null;
-        for (Metric metric: metricFileMap.keySet()) {
-            int dist = Metric.getDist(currentMetric, metric);
-            if (dist < minDist) {
-                minDist = dist;
-                result = metric;
+    private static void fillStore(String sourceDirectoryName) {
+        File sourceDirectory = new File(sourceDirectoryName);
+        if (!sourceDirectory.exists() || !sourceDirectory.isDirectory()) {
+            throw new RuntimeException(sourceDirectory.getAbsolutePath() + " не является директорией");
+        }
+        List<String> imageFileNames = new ArrayList<>();
+        for (File file : sourceDirectory.listFiles(file -> {
+            String fileName = file.getName().toUpperCase();
+            return fileName.contains("NEF") || fileName.contains("ARW");
+        })) {
+            imageFileNames.add(file.getAbsolutePath());
+        }
+        store = new Store();
+        for (String imageFileName : imageFileNames) {
+            store.add(new File(imageFileName), new File(getXmpFileName(imageFileName)));
+        }
+    }
+
+    private static void processImages(String imageDirectoryFileName) {
+        File imageDirectory = new File(imageDirectoryFileName);
+        if (!imageDirectory.exists() || !imageDirectory.isDirectory()) {
+            throw new RuntimeException(imageDirectory.getAbsolutePath() + " не является директорией");
+        }
+        for (File imageFile : imageDirectory.listFiles(file -> {
+            String fileName = file.getName().toUpperCase();
+            return fileName.contains("NEF") || fileName.contains("ARW");
+        })) {
+            XMPParams xmpParams = store.get(imageFile);
+            String xmpFileName = getXmpFileName(imageFile.getAbsolutePath());
+            if (!new File(xmpFileName).renameTo(new File(getXmpSourceFileName(xmpFileName)))) {
+                throw new RuntimeException("Не удалось переименовать файл " + xmpFileName);
+            }
+            try {
+                FileWriter fileWriter = new FileWriter(xmpFileName);
+                fileWriter.write(XMPParser.getReplacedFileContent(new File(getXmpSourceFileName(xmpFileName)), xmpParams));
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Ошибка записи файла " + imageFile.getAbsolutePath());
             }
         }
-        return result;
     }
 
-    private static BufferedImage getImage(String fileName) {
-        try {
-            File file = new File(fileName);
-            return ImageIO.read(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    private static String getXmpFileName(String imageFileName) {
+        String[] fileParts = imageFileName.split("\\.");
+        fileParts[fileParts.length - 1] = "xmp";
+        return String.join(".", fileParts);
+    }
+
+    private static String getXmpSourceFileName(String xmpFileName) {
+        String[] fileParts = xmpFileName.split("\\.");
+        fileParts[fileParts.length - 2] = fileParts[fileParts.length - 2] + "-s";
+        fileParts[fileParts.length - 1] = "xmp";
+        return String.join(".", fileParts);
     }
 }
--- src/main/java/XMPParser.java
+++ src/main/java/XMPParser.java
@@ -3,7 +3,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class XMPParser {
@@ -26,52 +28,112 @@ public class XMPParser {
 
     static {
         propertyToMethodMap = new HashMap<String, String>();
-        propertyToMethodMap.put(CRS_TEMPERATURE, "setTemperature");
-        propertyToMethodMap.put(CRS_TINT, "setTint");
-        propertyToMethodMap.put(CRS_EXPOSURE, "setExposure");
-        propertyToMethodMap.put(CRS_CONTRAST, "setContrast");
-        propertyToMethodMap.put(CRS_HIGHLIGHTS, "setHighlights");
-        propertyToMethodMap.put(CRS_SHADOWS, "setShadows");
-        propertyToMethodMap.put(CRS_WHITES, "setWhites");
-        propertyToMethodMap.put(CRS_BLACKS, "setBlacks");
-        propertyToMethodMap.put(CRS_CLARITY, "setClarity");
-        propertyToMethodMap.put(CRS_DEHAZE, "setDehaze");
-        propertyToMethodMap.put(CRS_VIBRANCE, "setVibrance");
-        propertyToMethodMap.put(CRS_SATURATION, "setSaturation");
+        propertyToMethodMap.put(CRS_TEMPERATURE, "Temperature");
+        propertyToMethodMap.put(CRS_TINT, "Tint");
+        propertyToMethodMap.put(CRS_EXPOSURE, "Exposure");
+        propertyToMethodMap.put(CRS_CONTRAST, "Contrast");
+        propertyToMethodMap.put(CRS_HIGHLIGHTS, "Highlights");
+        propertyToMethodMap.put(CRS_SHADOWS, "Shadows");
+        propertyToMethodMap.put(CRS_WHITES, "Whites");
+        propertyToMethodMap.put(CRS_BLACKS, "Blacks");
+        propertyToMethodMap.put(CRS_CLARITY, "Clarity");
+        propertyToMethodMap.put(CRS_DEHAZE, "Dehaze");
+        propertyToMethodMap.put(CRS_VIBRANCE, "Vibrance");
+        propertyToMethodMap.put(CRS_SATURATION, "Saturation");
 
         methodParametersMap = new HashMap<String, Class>();
-        methodParametersMap.put("setTemperature", int.class);
-        methodParametersMap.put("setTint", int.class);
-        methodParametersMap.put("setExposure", double.class);
-        methodParametersMap.put("setContrast", int.class);
-        methodParametersMap.put("setHighlights", int.class);
-        methodParametersMap.put("setShadows", int.class);
-        methodParametersMap.put("setWhites", int.class);
-        methodParametersMap.put("setBlacks", int.class);
-        methodParametersMap.put("setClarity", int.class);
-        methodParametersMap.put("setDehaze", int.class);
-        methodParametersMap.put("setVibrance", int.class);
-        methodParametersMap.put("setSaturation", int.class);
+        methodParametersMap.put("Temperature", int.class);
+        methodParametersMap.put("Tint", int.class);
+        methodParametersMap.put("Exposure", double.class);
+        methodParametersMap.put("Contrast", int.class);
+        methodParametersMap.put("Highlights", int.class);
+        methodParametersMap.put("Shadows", int.class);
+        methodParametersMap.put("Whites", int.class);
+        methodParametersMap.put("Blacks", int.class);
+        methodParametersMap.put("Clarity", int.class);
+        methodParametersMap.put("Dehaze", int.class);
+        methodParametersMap.put("Vibrance", int.class);
+        methodParametersMap.put("Saturation", int.class);
     }
 
-    public static XMPParams parse(File file) throws IOException {
-        XMPParams xmpParams = new XMPParams();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String line = reader.readLine();
-        while (line != null) {
-            addXMPParams(xmpParams, line);
-            line = reader.readLine();
+    public static XMPParams parse(File file) {
+        try {
+            XMPParams xmpParams = new XMPParams();
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                addXMPParams(xmpParams, line);
+                line = reader.readLine();
+            }
+            reader.close();
+            return xmpParams;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
         }
-        return xmpParams;
+    }
+
+    public static String getReplacedFileContent(File sourceFile, XMPParams xmpParams) {
+        try {
+            StringBuilder fileContent = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
+            List<String> replacedProperties = new ArrayList<>();
+            String line = reader.readLine();
+            String nextLine = reader.readLine();
+            while (!line.contains("crs:") || nextLine.contains("crs:")) {
+                fileContent.append(getReplacedLine(line, xmpParams, replacedProperties)).append("\"\n");
+                line = nextLine;
+                nextLine = reader.readLine();
+            }
+            for (String property: propertyToMethodMap.keySet()) {
+                if (!replacedProperties.contains(property)) {
+                    try {
+                        String methodName = "get" + propertyToMethodMap.get(property);
+                        Method method = xmpParams.getClass().getMethod(methodName);
+                        String propertyValue = method.invoke(xmpParams, null).toString();
+                        fileContent.append(String.format("   %s=\"%s\"\n", property, propertyValue));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+            while (line != null) {
+                fileContent.append(line).append("\n");
+                line = reader.readLine();
+            }
+            reader.close();
+            return fileContent.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private static String getReplacedLine(String line, XMPParams xmpParams, List<String> replacedProperties) {
+        for (String property : propertyToMethodMap.keySet()) {
+            if (line.lastIndexOf(property) != -1) {
+                try {
+                    replacedProperties.add(property);
+                    String methodName = "get" + propertyToMethodMap.get(property);
+                    Method method = xmpParams.getClass().getMethod(methodName);
+                    String[] lineParts = line.split("\"");
+                    lineParts[lineParts.length - 1] = method.invoke(xmpParams, null).toString();
+                    return String.join("\"", lineParts);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return line;
     }
 
     private static void addXMPParams(XMPParams xmpParams, String line) {
-        for (String property: propertyToMethodMap.keySet()) {
+        for (String property : propertyToMethodMap.keySet()) {
             if (line.lastIndexOf(property) != -1) {
                 try {
                     String methodName = propertyToMethodMap.get(property);
-                    Class paramClass =  methodParametersMap.get(methodName);
-                    Method method = xmpParams.getClass().getMethod(methodName, paramClass);
+                    Class paramClass = methodParametersMap.get(methodName);
+                    Method method = xmpParams.getClass().getMethod("set" + methodName, paramClass);
                     String param = line.split("\"")[1];
                     if (double.class.equals(paramClass)) {
                         method.invoke(xmpParams, Double.parseDouble(param));
